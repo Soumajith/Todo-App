24c27b68ff235f473b3fe3ef64550c7acd17fe0e
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-07-10 18:37:17 +0530

    Added delete a todo

--- controllers/deleteTodo.js
+++ controllers/deleteTodo.js
@@ -3,8 +3,17 @@ const Todo = require("../models/todo");
 exports.deleteTodo = async (request, response) => {
   try {
     const id = request.params.id;
-    const todo = await Todo.findByIdAndDelete({ _id: id });
+    await Todo.findByIdAndDelete(id);
+
+    response.status(200).json({
+      success: true,
+      message: "Todo Deleted",
+    });
   } catch (err) {
     console.log(err);
+    response.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
 };
--- routes/todo.js
+++ routes/todo.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const { createTodo } = require("../controllers/createTodo");
 const { getAllTodo, getTodoById } = require("../controllers/getAllTodo");
 const { updateTodo } = require("../controllers/updateTodo");
+const { deleteTodo } = require("../controllers/deleteTodo");
 
 router.post("/createTodo", createTodo);
 router.get("/getTodos", getAllTodo);
 router.get("/getTodos/:id", getTodoById);
 router.put("/updateTodo/:id", updateTodo);
+router.delete("/deleteTodo/:id", deleteTodo);
 
 module.exports = router;
