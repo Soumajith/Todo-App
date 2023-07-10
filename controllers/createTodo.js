24151bae5293596ed6a9fa657827d5873ba1d415
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-07-10 13:35:10 +0530

    Created Controller for create a todo

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -2,4 +2,24 @@ const Todo = require("../models/todo");
 
 //Route handler
 
-exports.createTodo = async(request, response){}
+exports.createTodo = async (request, response) => {
+  try {
+    // Fetch data
+    const { title, description } = request.body;
+    // create database for data
+    const data = await Todo.create({ title, description });
+    // create a response
+    response.status(200).json({
+      success: true,
+      data: data,
+      message: "Data successfully created",
+    });
+  } catch (e) {
+    console.log(err);
+    response.status(500).json({
+      success: false,
+      data: "Internal Server error",
+      message: e.message,
+    });
+  }
+};
